ac879260ec98533540f0d6b5b505867a31b2fb08
Frankc <63744530+francescocirulli@users.noreply.github.com>
2022-10-30 00:02:08 +0200

    wip

--- contracts/POGM.sol
+++ contracts/POGM.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./POGMRegistry.sol";
 
 /// @title    POGM (Proof Of Github Membership) token contract
 /// @author   GitGate - developed by @francescocirulli
@@ -28,7 +29,8 @@ contract POGMToken is ERC721, Ownable, EIP712, ERC721Votes, ERC721Burnable {
     Counters.Counter private _tokenIdCounter;
 
     error Already_Has_POGM(uint256 POGMId);
-    error Doesnt_Has_Requirements();
+    error Missing_Requirements();
+    error Address_Blacklisted();
 
     // modifier used to check if the msg.sender is the POGMRegistry address
     modifier onlyRegistry() {
@@ -50,19 +52,35 @@ contract POGMToken is ERC721, Ownable, EIP712, ERC721Votes, ERC721Burnable {
         tokenizedRepoId = _tokenizedRepoId;
         POGMRegistryAddress = _POGMRegistry;
         baseUri = _uri;
+        _tokenIdCounter.increment();
     }
 
  /**
-   * @dev this function can be used by an address to mint its POGM soulbound token. It must respect the tokenized repo requirements defined in the POGMRegistry
-   * @param receiver The address receiver of the POGM token
+   * @dev This function can be used by an address to mint its POGM soulbound token
+   * @dev It must respect the tokenized repo requirements defined in the POGMRegistry
+   * @dev An holder can't have more than one POGM
+   * @param _receiver The address receiver of the POGM token
    */
-    function safeMint(address receiver) public {
+    function safeMint(address _receiver) public {
+        address receiver = _receiver != address(0) ? _receiver : msg.sender;
+
         if (POGMHolders[receiver] != 0) revert Already_Has_POGM(POGMHolders[receiver]);
-        //if (POGMRegistry.checkUserRequirements(receiver)) revert Doesnt_Has_Requirements();
+
+        POGMRegistry instanceRegistry = POGMRegistry(POGMRegistryAddress);
+
+        address[] memory blacklisted = instanceRegistry.getBlacklistedAddresses(tokenizedRepoId);
+
+        (bool authorized) = _checkBlacklist(receiver, blacklisted);
+
+        if (!authorized) revert Address_Blacklisted();
+
+        if (!instanceRegistry.checkUserRequirements(tokenizedRepoId, receiver)) revert Missing_Requirements();
     
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(receiver, tokenId);
+
+        POGMHolders[receiver] = tokenId;
     }
 
  /**
@@ -71,8 +89,11 @@ contract POGMToken is ERC721, Ownable, EIP712, ERC721Votes, ERC721Burnable {
    * @param tokenId The POGM id to burn
    */
     function burn(uint256 tokenId) public override {
-        require(_isApprovedOrOwner(_msgSender(), tokenId) || msg.sender == POGMRegistryAddress, "ERC721: caller is not token owner or approved");
+        require(_isApprovedOrOwner(msg.sender, tokenId) || msg.sender == POGMRegistryAddress, "ERC721: caller is not token owner or approved");
+        
+        address owner = ownerOf(tokenId);
         _burn(tokenId);
+        delete POGMHolders[owner];
     }
 
     // The following function is an override required by Solidity
@@ -85,6 +106,16 @@ contract POGMToken is ERC721, Ownable, EIP712, ERC721Votes, ERC721Burnable {
       require(from == address(0) || to == address(0), "this token is a soulbound");
       super._beforeTokenTransfer(from, to, tokenId);
    }
+
+   // The following function is used to check if an address had been blacklisted on the POGM Registry
+   function _checkBlacklist(address addressReceiver, address[] memory blacklisted) internal pure returns (bool auth) {    
+        for (uint256 i = 0; i < blacklisted.length; i++) {
+            if (addressReceiver == blacklisted[i]) {
+                return false;
+            }   
+        }
+        return true;
+    }
 
     // The following function is an override in order to return the same baseUri string for all POGM ids of this contract
     function _baseURI() internal view override returns (string memory) {
